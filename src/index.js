e723fcdb5905787602c005f1d44828540dd6be42
Logan Diemer <logandiemer@gmail.com>
2026-08-25 21:26:02 -0400

    feat: add initial Node entry point

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const appName = "LoadForge";
+
+console.log(`${appName} is running.`);
+console.log(`Node version: ${process.version}`);
+console.log(`Platform: ${process.platform}`);
+console.log(`Process ID: ${process.pid}`);
+console.log(`Working directory: ${process.cwd()}`);
